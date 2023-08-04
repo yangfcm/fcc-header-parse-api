95925d2ce90d9e193605472ac2ff627f96f70722
Fan Yang <yangfcm16@gmail.com>
2023-08-04 22:21:15 +0930

    Get relevant client information.

--- controllers/whoami.js
+++ controllers/whoami.js
@@ -1,5 +1,12 @@
 const whoami = (req, res) => {
-  res.json("ok!");
+  const ip = req.ip || req.connection.remoteAddress;
+  const language = req.get("accept-language");
+  const software = req.get("user-agent");
+  res.json({
+    ip,
+    language,
+    software,
+  });
 };
 
 module.exports = { whoami };
